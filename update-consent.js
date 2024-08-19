8b1902e88f21b7eb50a82566df2cdd3ec949811b
Alejandro Clavero <alejandro.clavero@shopify.com>
2024-08-19 15:07:22 +0200

    Show region

--- update-consent.js
+++ update-consent.js
@@ -5,13 +5,23 @@ const bulbConsentSaleOfData = document.getElementById('consent-s');
 const greenCSS = 'saturate(50) hue-rotate(87deg)';
 const redCSS = 'saturate(50) hue-rotate(338deg)';
 
+const regionFlag = document.getElementById('region-flag');
+
+function countryEmoji(countryCode) {
+    if (countryCode.length < 2) {
+        return "🌍";
+    }
+
+    return countryCode.substring(0,2).toUpperCase().replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397));
+}
+
 function getConsentColor(val){
     if(val == 'no') return redCSS;
     if(val == 'yes') return greenCSS;
     return '';
 }
 
-function updateBulbs(){
+function updateConsent(){
     if(!window.Shopify.customerPrivacy) return;
 
     const consent = window.Shopify.customerPrivacy.currentVisitorConsent()
@@ -20,6 +30,11 @@ function updateBulbs(){
     bulbConsentAnalytics.style.filter = getConsentColor(consent.analytics);
     bulbConsentPreferences.style.filter = getConsentColor(consent.preferences);
     bulbConsentSaleOfData.style.filter = getConsentColor(consent.sale_of_data);
+
+    const region = window.Shopify.customerPrivacy.getRegion();
+
+    regionFlag.innerText = (region || "unknown") + " " + countryEmoji(region);
+
 }
 
-setInterval(updateBulbs, 300);
+setInterval(updateConsent, 300);
